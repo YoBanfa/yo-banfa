c20509de55596f543b703b6f6cf4e1040bfe1349
Hack Reactor Students <accounts@hackreactor.com>
2014-07-21 16:04:06 -0700

    add login controller& update gulp

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,4 +10,38 @@ angular.module('starter.controllers', ['starter.frontPage', 'starter.friends', '
   //   console.log('Going to ' + page);
   //   $location.url('/' + page);
   // };
+})
+.controller('LoginController', function ($scope, User) {
+  $scope.navTitle = 'Log In';
+
+  $scope.login = function(){
+    openFB.login('public_profile', function(){
+      // a little janky, but works for now
+      window.location.href = window.location.origin + '/#/chats';
+      User.userData();
+      $scope.loginMain();
+    },
+    function(err){
+      // maybe just throw this error?
+      console.log(err);
+    });
+  };
+
+})
+.controller('UserController', function ($scope) {
+  // somehow scope should be updated to have facebook name?
+  // I think
+  $scope.$root.loggedIn = false;
+
+  $scope.logout = function(){
+    openFB.logout();
+    window.sessionStorage.clear();
+    $scope.$root.loggedIn = false;
+    console.log($scope.$root.loggedIn);
+  };
+
+  $scope.loginMain = function(){
+    $scope.$root.loggedIn = true;
+  };
+
 });
