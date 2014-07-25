b768e3df49260345d93799d6438ab5028f0d4bb1
jlburkhead <jlburkhead@ucdavis.edu>
2014-07-25 12:22:07 -0700

    refactor removal of game from users' currentGames

--- server/api/games/gameController.js
+++ server/api/games/gameController.js
@@ -79,15 +79,25 @@ module.exports = {
         if (game.creatorScore > -1 && game.challengedScore > -1){
           Game.update(conditions, {complete: true}, function(err, numupdated){
             if (err){ console.log(err); }
-            User.findOne({ facebookId: game.creator }, function(err, user){
-              var index = user.currentGames.indexOf(game._id);
-              user.currentGames.splice(index, 1);
-              user.save(function(err){ err && console.log(err); });
-            });
-            User.findOne({ facebookId: game.challenged }, function(err, user){
-              var index = user.currentGames.indexOf(game._id);
-              user.currentGames.splice(index, 1);
-              user.save(function(err) { err && console.log(err); });
+            User.find({
+              facebookId: { 
+                $in: [
+                  game.creator,
+                  game.challenged
+                ]
+              }
+            }, function(err, users){
+              if (err) { 
+                console.log(err); 
+              } else {
+                users.forEach(function(user){
+                  var index = user.currentGames.indexOf(game._id);
+                  user.currentGames.splice(index, 1);
+                  user.save(function(err){
+                    if (err) { console.log(err); }
+                  });
+                });
+              }
             });
             res.json(game);
           });
